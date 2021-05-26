5e05856e5709339bcd7031736d07a7f18b439f44
Kobayashi Takahiro <koba925@gmail.com>
2021-05-27 08:11:18 +0900

    Implemented several S-exp functions

--- src/sexp.js
+++ src/sexp.js
@@ -4,13 +4,25 @@ class SXPValue {
   }
 
   toString () {
-    return this.val.toString()
+    if (isNull(this)) {
+      return '()'
+    } else if (typeof this.val === 'boolean') {
+      return this.val ? '#t' : '#f'
+    } else if (typeof this.val === 'string') {
+      return '"' + this.val.toString() + '"'
+    } else {
+      return this.val.toString()
+    }
   }
 }
 
 const makeValue = a => new SXPValue(a)
 const isValue = a => a instanceof SXPValue
 const valueOf = a => a.val
+const eqv = (a, b) => isValue(a) && isValue(b) && a.val === b.val
+
+const NULL = new SXPValue(null)
+const isNull = a => isValue(a) && valueOf(a) === null
 
 class SXPSymbol {
   constructor (name) {
@@ -26,11 +38,123 @@ const makeSymbol = a => new SXPSymbol(a)
 const isSymbol = a => a instanceof SXPSymbol
 const nameOf = a => a.name
 
+class SXPPair {
+  constructor (car, cdr) {
+    this.car = car
+    this.cdr = cdr
+  }
+
+  toString () {
+    const inside = l => {
+      if (isNull(cdr(l))) return car(l).toString()
+      if (isPair(cdr(l))) {
+        return car(l).toString() + ' ' + inside(cdr(l))
+      }
+      return car(l).toString() + ' . ' + cdr(l).toString()
+    }
+    return `(${inside(this)})`
+  }
+}
+
+const makePair = (car, cdr) => new SXPPair(car, cdr)
+const isPair = a => a instanceof SXPPair
+const isAtom = a => !isPair(a) && !isNull(a)
+const cons = (car, cdr) => makePair(car, cdr)
+const car = a => a.car
+const cdr = a => a.cdr
+const setCar = (a, val) => { a.car = val }
+const setCdr = (a, val) => { a.cdr = val }
+const equals = (a, b) => {
+  if (isValue(a) && isValue(b)) return eqv(a, b)
+  if (isSymbol(a) && isSymbol(b)) return nameOf(a) === nameOf(b)
+  if (!isPair(a) || !isPair(b)) return false
+  return equals(car(a), car(b)) && equals(cdr(a), cdr(b))
+}
+
+const toString = a => a.toString()
+
+const skipWhite = str => {
+  return str.replace(/^\s*/, '')
+}
+
+const expect = (str, start) => {
+  str = skipWhite(str)
+  if (!str.startsWith(start)) {
+    throw new SyntaxError(`Expected "${start}" at "${str}"`)
+  }
+  return str.slice(start.length)
+}
+
+const expectEnd = str => {
+  str = skipWhite(str)
+  if (str !== '') throw new SyntaxError(`Extra chars at "${str}"`)
+}
+
+const parseList = str => {
+  str = skipWhite(str)
+  if (str.startsWith(')')) return [str.slice(1), NULL]
+
+  let a, d
+  [str, a] = parseTerm(str)
+
+  str = skipWhite(str)
+
+  if (str.startsWith('.')) {
+    str = str.slice(1)
+    ;[str, d] = parseTerm(str)
+    str = expect(str, ')')
+    return [str, cons(a, d)]
+  }
+
+  [str, d] = parseList(str)
+  return [str, cons(a, d)]
+}
+
+const parseString = str => {
+  const chars = str.match(/^"([^"]*)"/)[1]
+  return [str.slice(chars.length + 2), makeValue(chars)]
+}
+
+const parseTerm = str => {
+  str = skipWhite(str)
+  if (str.startsWith('(')) return parseList(str.slice(1))
+  if (str.startsWith('"')) return parseString(str)
+
+  const token = str.match(/[^\s().]+/)[0]
+  if (token === '#t' || token === '#f') {
+    return [str.slice(token.length), makeValue(token === '#t')]
+  }
+  if (/^\d+$/.test(token)) {
+    return [str.slice(token.length), makeValue(Number(token))]
+  }
+  return [str.slice(token.length), makeSymbol(token)]
+}
+
+const toSexp = str => {
+  let exp
+  [str, exp] = parseTerm(str)
+  expectEnd(str)
+  return exp
+}
+
 module.exports = {
   makeValue,
   isValue,
   valueOf,
+  eqv,
   makeSymbol,
+  NULL,
+  isNull,
   isSymbol,
-  nameOf
+  nameOf,
+  isPair,
+  isAtom,
+  cons,
+  car,
+  cdr,
+  setCar,
+  setCdr,
+  equals,
+  toString,
+  toSexp
 }
--- test/sexp.test.js
+++ test/sexp.test.js
@@ -1,30 +1,232 @@
 /* eslint-env jest */
 
 const {
-  makeValue, isValue, valueOf,
-  makeSymbol, isSymbol, nameOf
+  makeValue, isValue, valueOf, eqv,
+  NULL, isNull,
+  makeSymbol, isSymbol, nameOf,
+  isPair, isAtom, cons, car, cdr, setCar, setCdr, equals,
+  toString, toSexp
 } = require('../src/sexp')
 
-test('The value of value(1) is 1', () => {
-  expect(valueOf(makeValue(1))).toBe(1)
+describe('Test for values', () => {
+  test('ValueOf returns the correct value for number', () => {
+    expect(valueOf(makeValue(1))).toBe(1)
+  })
+
+  test('A number is a value', () => {
+    expect(isValue(makeValue(1))).toBe(true)
+  })
+
+  test('A boolean is a value', () => {
+    expect(isValue(makeValue(true))).toBe(true)
+  })
+
+  test('A string is a value', () => {
+    expect(isValue(makeValue('str'))).toBe(true)
+  })
+
+  test('A symbol is not a value', () => {
+    expect(isValue(makeSymbol('a'))).toBe(false)
+  })
+
+  test('1 is eqv to 1', () => {
+    expect(eqv(makeValue(1), makeValue(1))).toBe(true)
+  })
+
+  test('1 is not eqv to 2', () => {
+    expect(eqv(makeValue(1), makeValue(2))).toBe(false)
+  })
+
+  test('1 is not eqv to "1"', () => {
+    expect(eqv(makeValue(1), makeValue('1'))).toBe(false)
+  })
+
+  test('1 is not eqv to symbol "a"', () => {
+    expect(eqv(makeValue(1), makeSymbol('a'))).toBe(false)
+  })
+
+  test('Symbol "a" is not eqv to 1', () => {
+    expect(eqv(makeSymbol('a'), makeValue(1))).toBe(false)
+  })
 })
 
-test('Value(1) is a value', () => {
-  expect(isValue(makeValue(1))).toBe(true)
+describe('Test for null', () => {
+  test('NULL is null', () => {
+    expect(isNull(NULL)).toBe(true)
+  })
+
+  test('A number is not null', () => {
+    expect(isNull(makeValue(1))).toBe(false)
+  })
 })
 
-test('Symbol("a") is not a value', () => {
-  expect(isValue(makeSymbol('a'))).toBe(false)
+describe('Test for symbols', () => {
+  test('NameOf returns the name of the symbol', () => {
+    expect(nameOf(makeSymbol('sym'))).toBe('sym')
+  })
+
+  test('A symbol is a symbol', () => {
+    expect(isSymbol(makeSymbol('sym'))).toBe(true)
+  })
+
+  test('A value is not a symbol', () => {
+    expect(isSymbol(makeValue(1))).toBe(false)
+  })
 })
 
-test('The name of Symbol("a") is "a"', () => {
-  expect(nameOf(makeSymbol('a'))).toBe('a')
+describe('Test for conses', () => {
+  test('cons is a pair', () => {
+    expect(isPair(
+      cons(makeSymbol('a'), makeSymbol('b'))
+    )).toBe(true)
+  })
+  test('Symbol is not a pair', () => {
+    expect(isPair(makeSymbol('a'))).toBe(false)
+  })
+
+  test('A number is an atom', () => {
+    expect(isAtom(makeValue(1))).toBe(true)
+  })
+  test('A cons is not an atom', () => {
+    expect(isAtom(
+      cons(makeSymbol('a'), makeSymbol('b'))
+    )).toBe(false)
+  })
+  test('NULL is not an atom', () => {
+    expect(isAtom(NULL)).toBe(false)
+  })
+
+  test('car(cons(a b)) equals a', () => {
+    expect(car(cons(makeSymbol('a'), makeSymbol('b')))).toStrictEqual(makeSymbol('a'))
+  })
+
+  test('cdr(cons(a b)) equals b', () => {
+    expect(cdr(cons(makeSymbol('a'), makeSymbol('b')))).toStrictEqual(makeSymbol('b'))
+  })
+
+  test('SetCar sets the car of cons', () => {
+    const a = cons(makeValue(1), makeValue(2))
+    setCar(a, 3)
+    expect(a.car).toBe(3)
+  })
+
+  test('SetCdr sets the cdr of cons', () => {
+    const a = cons(makeValue(1), makeValue(2))
+    setCdr(a, 3)
+    expect(a.cdr).toBe(3)
+  })
 })
 
-test('Symbol("a") is a symbol', () => {
-  expect(isSymbol(makeSymbol('a'))).toBe(true)
+describe('Test for equals', () => {
+  test('1 equals 1', () => {
+    expect(equals(makeValue(1), makeValue(1))).toBe(true)
+  })
+  test('Null equals null', () => {
+    expect(equals(NULL, NULL)).toBe(true)
+  })
+  test('1 not equals NULL', () => {
+    expect(equals(makeValue(1), NULL)).toBe(false)
+  })
+  test('Symbol "a" equals symbol "a"', () => {
+    expect(equals(makeSymbol('a'), makeSymbol('a'))).toBe(true)
+  })
+  test('Symbol "a" not equals symbol "b"', () => {
+    expect(equals(makeSymbol('a'), makeSymbol('b'))).toBe(false)
+  })
+  test('Symbol "a" not equals 1', () => {
+    expect(equals(makeSymbol('a'), makeValue(1))).toBe(false)
+  })
+  test('(1 . 2) equals (1 . 2)', () => {
+    expect(equals(
+      cons(makeValue(1), makeValue(2)),
+      cons(makeValue(1), makeValue(2))
+    )).toBe(true)
+  })
+  test('(1 . 2) equals (1 . 3)', () => {
+    expect(equals(
+      cons(makeValue(1), makeValue(2)),
+      cons(makeValue(1), makeValue(3))
+    )).toBe(false)
+  })
 })
 
-test('Value(1) is not a symbol', () => {
-  expect(isSymbol(makeValue(1))).toBe(false)
+describe('ToString', () => {
+  it('makes a correct string of a number', () => {
+    expect(toString(makeValue(1))).toBe('1')
+  })
+
+  it('makes a correct string of a string', () => {
+    expect(toString(makeValue('str'))).toBe('"str"')
+  })
+
+  it('makes a correct string of a boolean', () => {
+    expect(toString(makeValue(true))).toBe('#t')
+    expect(toString(makeValue(false))).toBe('#f')
+  })
+
+  it('makes a correct string of null', () => {
+    expect(toString(NULL)).toBe('()')
+  })
+
+  it('makes a correct string of a symbol', () => {
+    expect(toString(makeSymbol('sym'))).toBe('sym')
+  })
+
+  it('makes a correct string of a cons', () => {
+    expect(toString(toSexp('(sym 1)'))).toBe('(sym 1)')
+    expect(toString(toSexp('(sym . 1)'))).toBe('(sym . 1)')
+  })
+})
+
+describe('ToSexp', () => {
+  it('returns 1 when given "1"', () => {
+    expect(toSexp('1')).toStrictEqual(makeValue(1))
+  })
+  it('returns 1 when given " 1"', () => {
+    expect(toSexp(' 1')).toStrictEqual(makeValue(1))
+  })
+  it('returns 1 when given "1 "', () => {
+    expect(toSexp(' 1')).toStrictEqual(makeValue(1))
+  })
+
+  it('returns boolean true when given "#t"', () => {
+    expect(toSexp('#t')).toStrictEqual(makeValue(true))
+  })
+  it('returns boolean false when given "#f"', () => {
+    expect(toSexp('#f')).toStrictEqual(makeValue(false))
+  })
+
+  it('returns string "aaa" when given "aaa"', () => {
+    expect(toSexp('"aaa"')).toStrictEqual(makeValue('aaa'))
+  })
+
+  it('returns symbol "1a" when given "1a"', () => {
+    expect(toSexp('1a')).toStrictEqual(makeSymbol('1a'))
+  })
+
+  it('returns NULL when given "()"', () => {
+    expect(toSexp('()')).toStrictEqual(NULL)
+  })
+  it('returns NULL when given "( )"', () => {
+    expect(toSexp('( )')).toStrictEqual(NULL)
+  })
+  it('returns (1.2) when given "(1 . 2)"', () => {
+    expect(toSexp('(1.2)')).toStrictEqual(
+      cons(makeValue(1), makeValue(2))
+    )
+  })
+  it('returns (1 . 2) when given "(1 . 2)"', () => {
+    expect(toSexp('(1 . 2)')).toStrictEqual(
+      cons(makeValue(1), makeValue(2))
+    )
+  })
+  it('returns (1 2) when given "(1 2)"', () => {
+    expect(toSexp('(1 2)')).toStrictEqual(
+      cons(makeValue(1), cons(makeValue(2), NULL))
+    )
+  })
+
+  it('throws SytaxError when given "1 1"', () => {
+    expect(() => toSexp('1 1')).toThrowError(SyntaxError)
+  })
 })
